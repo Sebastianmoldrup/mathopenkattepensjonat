6399cdaa7c28f40ebf4f440b8ff80e8ae43bba70
sebastian <sebastianmoldrup@Sebastians-Mac-mini.local>
2026-01-17 21:40:55 +0100

    deploy issue, removed unused component

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -35,7 +35,6 @@ export default function ProtectedPage() {
       </div>
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
