954a7ada4df9847e5959d1de625d3d13b52d1bbd
Sebastian Møldrup <sebastianmoldrup@Mac.home>
2026-02-09 23:39:09 +0100

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const CARD_CONTENT: CardContent[] = [
   {
     img: "playing-no-bg",
     title: "Romslige fasiliteter",
-    desc: "Romslige bur, kattegård åpen hele dagen, veggmonterte kattemøbler og gode aktivitetsområdet",
+    desc: "Romslige bur, kattegård åpen hele dagen, veggmonterte kattemøbler og gode aktivitetsområder",
   },
   {
     img: "phone-cat-playing-no-bg",
