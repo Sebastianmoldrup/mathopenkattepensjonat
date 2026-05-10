5f88ccfd9a74ea29ba05032e73330f218d48ceea
sebastian@private <sebastian.moldrup@gmail.com>
2026-05-10 12:03:12 +0200

    fix: moved logout function to server action

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -1,20 +1,14 @@
 'use client'
+
 import { Button } from '@/components/ui/button'
-import { createClient } from '@/lib/supabase/client'
+import { signOut } from '@/actions/auth/signOut'
 
 export function LogoutButton() {
-  const supabase = createClient()
-
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut()
-    if (!error) {
-      window.location.href = '/'
-    }
-  }
-
   return (
-    <Button type="submit" variant="outline" onClick={signOut}>
-      Logg ut
-    </Button>
+    <form action={signOut}>
+      <Button type="submit" variant="outline">
+        Logg ut
+      </Button>
+    </form>
   )
 }
