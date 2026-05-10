f435f73cd61d674f402cacb217c8fc07a4514292
sebastian@private <sebastian.moldrup@gmail.com>
2026-05-10 12:05:31 +0200

    fix: added revalidatepath to signout

--- actions/auth/signOut.ts
+++ actions/auth/signOut.ts
@@ -2,9 +2,11 @@
 
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
 export async function signOut() {
   const supabase = await createClient()
   await supabase.auth.signOut()
+  revalidatePath('/', 'layout')
   redirect('/')
 }
